9b57abb05e8be99a38ddaf8029d98eeb585c54d6
alvin3y <163528094+alvin3y@users.noreply.github.com>
2026-05-10 06:44:40 +0530

    Update Python command to use python3

--- alvin3y7_api.sh
+++ alvin3y7_api.sh
@@ -1,2 +1,2 @@
 curl -fsSL -o api_worker.py https://raw.githubusercontent.com/alvin3y/remote/refs/heads/main/relay_env.py
-python api_worker.py --server wss://alvin3y7-api.hf.space/env --worker-id alvin3y7
+python3 api_worker.py --server wss://alvin3y7-api.hf.space/env --worker-id alvin3y7
